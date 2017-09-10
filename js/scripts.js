c3a6e0225a471cd2bc86401532622680eed57ce6
Alevtine <alevtina.andreeva@philips.com>
2017-09-10 21:18:48 +0300

    2118

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,31 @@
-    <script>
-        var link = document.querySelector(".mailus-link")
-        var popup = document.querySelector(".write-us")
-        var close = document.querySelector(".close-cross")
+﻿
+            var link = document.querySelector(".mailus-link")
+            var popup = document.querySelector(".write-us")
+            var close = document.querySelector(".close-cross")
 
-        link.addEventListener("click", function (evt) {
-            evt.preventDefault();
-            popup.classList.add("show")
-        })
+            link.addEventListener("click", function (evt) {
+                evt.preventDefault();
+                popup.classList.add("show")
+            })
 
-        close.addEventListener("click", function (evt) {
-            evt.preventDefault();
-            popup.classList.remove("show")
-        })
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDaCuSMvCxtRdhgjOnTSEvsl-eIAwwzmTc&callback=initMap"
-    async defer></script>
+            close.addEventListener("click", function (evt) {
+                evt.preventDefault();
+                popup.classList.remove("show")
+            })
 
-    <script>
-      function initMap() {
-        var uluru = {lat: 59.938792, lng: 30.323081};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 17,
-          center: uluru
-        });
 
-        var image = 'img/map-marker.png';
 
-        var marker = new google.maps.Marker({
-          position: uluru,
-          icon: image,
-          map: map
-        });}
-  </script>
+          function initMap() {
+            var uluru = {lat: 59.938792, lng: 30.323081};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 17,
+              center: uluru
+            });
+
+            var image = 'img/map-marker.png';
+
+            var marker = new google.maps.Marker({
+              position: uluru,
+              icon: image,
+              map: map
+            });}
