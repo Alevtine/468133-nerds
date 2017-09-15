f15377c8988260fb0f3f62024efbe84690c0a955
Alevtine <alevtina.andreeva@philips.com>
2017-09-15 17:28:21 +0300

    1728

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,54 @@
-﻿
-            var link = document.querySelector(".mailus-link")
-            var popup = document.querySelector(".write-us")
-            var close = document.querySelector(".close-cross")
+var link = document.querySelector(".mailus-link")
+var popup = document.querySelector(".write-us")
+var close = document.querySelector(".close-cross")
 
 
-            link.addEventListener("click", function (evt) {
-                evt.preventDefault();
-                popup.classList.add("show");
-                popup.classList.add("effect");
-            })
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.add("show");
+    popup.classList.add("effect");
+})
 
-            close.addEventListener("click", function (evt) {
-                evt.preventDefault();
-                popup.classList.remove("show");
-                popup.classList.remove("effect");
-            })
+close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("show");
+    popup.classList.remove("effect");
+})
 
-            var submitButton = document.querySelector('.write-us button');
-             submitButton.addEventListener("click", function (evt) {
-                            evt.preventDefault();
-                            popup.classList.remove("effect");
-                            popup.classList.add("form-error");
-                        });
+var submitButton = document.querySelector('.write-us button');
+submitButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("effect");
+    popup.classList.add("form-error");
+});
 
-            popup.submit(function(event) {
-              if (!input_name.val() || !input_email.val() || !input_message.val()) {
-                            event.preventDefault();
-                            popup.removeClass("effect");
-                            setTimeout(function() {
-                            popup.addClass("form-error") }, 50);
-              }
-            });
+var submitForm = document.querySelector('.write-us-inputs');
+submitForm.submit(function(event) {
+    if (!input_name.val() || !input_email.val() || !input_message.val()) {
+        event.preventDefault();
+        popup.removeClass("effect");
+        setTimeout(function() {
+            popup.addClass("form-error")
+        }, 50);
+    }
+});
 
 
-          function initMap() {
-            var uluru = {lat: 59.938792, lng: 30.323081};
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 17,
-              center: uluru
-            });
+function initMap() {
+    var uluru = {
+        lat: 59.938792,
+        lng: 30.323081
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: uluru
+    });
 
-            var image = 'img/map-marker.png';
+    var image = 'img/map-marker.png';
 
-            var marker = new google.maps.Marker({
-              position: uluru,
-              icon: image,
-              map: map
-            });}
+    var marker = new google.maps.Marker({
+        position: uluru,
+        icon: image,
+        map: map
+    });
+}
